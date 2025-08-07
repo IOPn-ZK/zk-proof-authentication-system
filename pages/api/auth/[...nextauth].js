c46213e7ec111f747782f58c4e0a63a3855374f7
Madhu Varsha <madhuvarsha0608@gmail.com>
2025-08-07 21:16:07 +0530

    callbacks added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -20,4 +20,12 @@ export default NextAuth({
     encryption: true
   },
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
+    },
+  },
+  debug: process.env.NODE_ENV === 'development',
 })
